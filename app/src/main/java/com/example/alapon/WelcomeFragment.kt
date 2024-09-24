a447d564f3c8871712e21078a382d0c31cc822cc
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-24 11:39:09 +0600

    Write code for go to WelcomeFragment to LoginFragment!

--- app/src/main/java/com/example/alapon/WelcomeFragment.kt
+++ app/src/main/java/com/example/alapon/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.alapon.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -16,6 +17,17 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+
+        binding.apply {
+
+            loginButton.setOnClickListener {
+
+                findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
+
+            }
+
+        }
+
         return binding.root
     }
 
