47e929bda554ea0dd316d29e02401c08379cb973
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-24 11:35:56 +0600

    Start using binding!

--- app/src/main/java/com/example/alapon/WelcomeFragment.kt
+++ app/src/main/java/com/example/alapon/WelcomeFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.alapon.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
