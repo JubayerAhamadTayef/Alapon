eb27dff17e03362c96cd6a81e0a26e6e8a363255
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-26 12:11:44 +0600

    Complete Sign Up!

--- app/src/main/java/com/example/alapon/SignUpFragment.kt
+++ app/src/main/java/com/example/alapon/SignUpFragment.kt
@@ -1,59 +1,98 @@
 package com.example.alapon
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.alapon.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        binding.apply {
+
+            signUpButton.setOnClickListener {
+
+                if (userEditText.text?.isNotEmpty() == true && emailEditText.text?.isNotEmpty() == true && passwordEditText.text?.isNotEmpty() == true) {
+
+                    val userName = userEditText.text.toString().trim()
+                    val email = emailEditText.text.toString().trim()
+                    val password = passwordEditText.text.toString().trim()
+
+                    if (isEmailValid(email) && isPasswordValid(password)) {
+
+                        signUpUser(userName, email, password)
+
+                    } else {
+
+                        Toast.makeText(requireContext(), "Invalid Email or Password!", Toast.LENGTH_SHORT).show()
+
+                    }
+
+
+                } else {
+
+                    Toast.makeText(requireContext(), "Please, provide needed Information, then click again Sign Up Button.", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+            alreadyHaveAccount.setOnClickListener {
+
+                findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+
+            }
+
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun isEmailValid(email: String): Boolean {
+
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+
+        val passRegex = Regex("^(?=.*[A-Za-z])(?=.*[@\$!%*#?&])[A-Za-z@\$!%*#?&\\d]{6,}\$")
+        return password.matches(passRegex)
+
+    }
+
+    private fun signUpUser(userName: String, email: String, password: String) {
+
+        val auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+
+            if (it.isSuccessful) {
+
+                Toast.makeText(requireContext(), "Sign Up Successfully, $email", Toast.LENGTH_SHORT).show()
+
+                findNavController().navigate(R.id.action_signUpFragment_to_homeFragment)
+
+            } else {
+
+                Toast.makeText(requireContext(), "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+
             }
+
+        }
+
     }
 }
